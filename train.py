d46f628fcee803a011d49fe1b9c9891da94b7fd4
Christopher Wang <czw@csail.mit.edu>
2021-07-07 04:37:29 -0400

    Add config file

--- train.py
+++ train.py
@@ -1,22 +1,32 @@
+import sys
 import os
 import gym
+import yaml #pip install pyyaml
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from vgdl.util.humanplay.play_vgdl import register_vgdl_env
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-def train(model, domain_file, level_file, observer = 'image', blocksize = 5, steps = 10000):
-    '''Training function, includes domain file with VGDL rules and designed level to render environment from.'''
-    env_name = register_vgdl_env(domain_file, level_file, observer, blocksize)
+arg_file = sys.argv[1]
+with open(arg_file, 'r') as f:
+    cfg = yaml.load(f, yaml.SafeLoader)
+
+def train(model, domain_file, level_file, 
+          observer = 'image', blocksize = 5, steps = 10000):
+    '''Training function, includes domain file with VGDL rules and 
+       designed level to render environment from.'''
+
+    env_name = register_vgdl_env(domain_file, level_file, 
+                                 observer, blocksize)
     env = gym.make(env_name)
     if observer=='image':
         env.render(mode='human')
+        env.close()
 
     model = PPO(model, env, verbose=1).learn(steps)
 
-domain_file = 'vgdl/games/aliens.txt'
-level_file = 'vgdl/games/aliens_lvl0.txt'
-model = 'MlpPolicy'
 
-train(model, domain_file, level_file, observer="features")
+train(cfg["model"], cfg["domain_file"], 
+      cfg["level_file"], observer=cfg["observer"],
+      steps=cfg["steps"])
